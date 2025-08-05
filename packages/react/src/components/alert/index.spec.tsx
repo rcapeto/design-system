50406faae9f1bca0d62df707a2e49743cd1b8ae0
Raphael Capeto <raphaelcapeto@gmail.com>
2025-08-05 10:26:38 -0300

    test: add alert coverage

---
+++ packages/react/src/components/alert/index.spec.tsx
@@ -0,0 +1,42 @@
+import '@testing-library/jest-dom'
+import { cleanup, fireEvent, render } from '@testing-library/react'
+import { Alert } from '~/components/alert'
+
+describe('Alert', () => {
+  beforeEach(() => {
+    cleanup()
+  })
+
+  it('should render button when buttonText is provided', async () => {
+    const buttonText = 'any-text'
+    const text = 'any-alert-text'
+
+    const { findByRole } = render(<Alert text={text} buttonText={buttonText} />)
+
+    const element = await findByRole('button')
+
+    expect(element).toBeInTheDocument()
+  })
+
+  it('should be able to click in button when button text is provided and onClick', async () => {
+    const buttonText = 'any-text'
+    const text = 'any-alert-text'
+    const handleClick = jest.fn()
+    const testID = 'alert'
+
+    const { findByRole } = render(
+      <Alert
+        text={text}
+        buttonText={buttonText}
+        onClick={handleClick}
+        data-testid={testID}
+      />,
+    )
+
+    const button = await findByRole('button')
+
+    fireEvent.click(button, handleClick)
+
+    expect(handleClick).toHaveBeenCalledTimes(1)
+  })
+})
